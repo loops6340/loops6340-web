f15af574b78af2690845d48c45446d74d94155bb
loops6340 <gonzalesinciofabioleonel@gmail.com>
2021-12-05 16:34:46 -0500

    aaa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 const path = require('path')
 
+//@ts-check
+/** @type {import("next").NextConfig} */
 module.exports = {
     sassOptions: {
       includePaths: [path.join(__dirname, 'styles')],
       prependData: `@import "~@styles/variables.scss";`,
     },
+    webpack: (config, {isServer}) => {
+       if (isServer) {
+            config.externals.push('_http_common');        
+       }
+       return config;
+    },
+    target: 'serverless',
 }
