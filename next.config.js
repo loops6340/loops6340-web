b5d1b0ce8b3c901fd8c7695b1812df04297b60a8
loops6340 <gonzalesinciofabioleonel@gmail.com>
2021-12-05 18:13:24 -0500

    aaa

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ module.exports = {
       prependData: `@import "~@styles/variables.scss";`,
     },
     webpack: (config, {isServer}) => {
+       const originalEntry = config.entry
+       config.entry = async () => {
+		const entries = { ...(await originalEntry()) }
+		entries['./scripts/build-rss'] = './scripts/build-rss.js'
+		return entries
+       }
        if (isServer) {
             config.externals.push('_http_common');        
        }
